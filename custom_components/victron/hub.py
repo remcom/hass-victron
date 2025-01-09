d2d8848152643c244f31942303986ed45033d41d
Remco van Essen <remco.essen@gmail.com>
2025-01-09 12:19:20 +0100

    Update read and write for pymodbus compatibility

--- custom_components/victron/hub.py
+++ custom_components/victron/hub.py
@@ -30,13 +30,17 @@ class VictronHub:
     def write_register(self, unit, address, value):
         with self._lock:
             slave = int(unit) if unit else 1
-            return self._client.write_register(address, value, slave=slave)
+            return self._client.write_register(
+                address=address, value=value, slave=slave
+            )
 
     def read_holding_registers(self, unit, address, count):
         """Read holding registers."""
         with self._lock:
             slave = int(unit) if unit else 1
-            return self._client.read_holding_registers(address, count, slave=slave)
+            return self._client.read_holding_registers(
+                address=address, count=count, slave=slave
+            )
 
     def calculate_register_count(self, registerInfoDict: OrderedDict):
         first_key = next(iter(registerInfoDict))
